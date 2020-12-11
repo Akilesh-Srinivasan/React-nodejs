8526881e04edf0682a8742845238fe96fe7baf0d
unknown <akilesh.srinivasan@SVUSLP00609.us.softvision.global>
2020-12-11 12:45:40 -0500

    changed the greeting

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'there'});
+    res.send({ hi: 'thereeee'});
 });
 //instructs node to listen to port 5000
 // environemnt variable has PORT or use 5000
 const PORT = process.env.PORT || 5000;
-app.listen(5000);
+app.listen(PORT, "0.0.0.0");
