4b13e117723e27a12df35b0a0feecc96a34fa86d
unknown <akilesh.srinivasan@SVUSLP00609.us.softvision.global>
2020-12-11 11:58:43 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+// require is similar to import statement
+
+// app object represents the route handler with get http request
+// res.send will send out the json data
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send({ hi: 'there'});
+});
+//instructs node to listen to port 5000
+// environemnt variable has PORT or use 5000
+const PORT = process.env.PORT || 5000;
+app.listen(5000);
